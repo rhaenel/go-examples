aa619a365b6e4dcea4cb853609cf0451642de3b5
Roland Haenel <roland@haenel.me>
2018-03-21 11:54:05 +0100

    functions example

--- functions/functions.go
+++ functions/functions.go
@@ -13,6 +13,20 @@ func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
+// functions can return multiple values
+func plusMinus(a, b int) (int, int) {
+	return a + b, a - b
+}
+
+// variable length argument list
+func sum(nums ...int) int {
+	sum := 0
+	for _, i := range nums {
+		sum += i
+	}
+	return sum
+}
+
 func main() {
 
 	// calling is just like you expect
@@ -21,4 +35,15 @@ func main() {
 
 	res = plusPlus(1, 2, 3)
 	fmt.Println("1+2+3 =", res)
+
+	p, m := plusMinus(1, 2)
+	fmt.Printf("1 +- 2 = %d, %d\n", p, m)
+
+	fmt.Printf("1+2+3+4+5 = %d\n", sum(1, 2, 3, 4, 5))
+
+	// anonymous functions used as a type
+	f := func(i int) int {
+		return i * 2
+	}
+	fmt.Printf("calling anonymous function: %d\n", f(2))
 }
